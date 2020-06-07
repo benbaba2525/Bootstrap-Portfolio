53768cbb937eb38955e8d4350e5666fb6effac70
Kanyarut Pornamnuay <benbaba2525@hotmail.com>
2020-06-07 01:17:51 -0700

    Added map to locate my address

---
+++ Javascript/map.js
@@ -0,0 +1,34 @@
+
+mapboxgl.accessToken = 'pk.eyJ1IjoiYmVuYmFiYTI1MjUiLCJhIjoiY2thbXc0MmFsMWl0NDJ2cXo0YzRqanR1ZyJ9.5sTRCYYLe8iSOjPnF84w7w';
+    var mapboxClient = mapboxSdk({ accessToken: mapboxgl.accessToken });
+    mapboxClient.geocoding
+        .forwardGeocode({
+            query: '5119 Fountain Ave, Los Angeles 90029, USA',
+            autocomplete: false,
+            limit: 1
+        })
+        .send()
+        .then(function(response) {
+            if (
+                response &&
+                response.body &&
+                response.body.features &&
+                response.body.features.length
+            ) {
+                var feature = response.body.features[0];
+
+                var map = new mapboxgl.Map({
+                    container: 'map',
+                    style: 'mapbox://styles/mapbox/streets-v11',
+                    center: feature.center,
+                    zoom: 10
+                });
+                new mapboxgl.Marker().setLngLat(feature.center).addTo(map);
+            }
+        });
+
+
+
+
+
+        
